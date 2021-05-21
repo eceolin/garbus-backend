7a8355690eb23d3563ec393ab2c9a53a50e84771
Mario Specht <ms.specht@gmail.com>
2021-05-21 20:55:59 -0300

    Inserido auth no swagger

--- src/main/java/pucrs/ages/garbus/Config/SpringFoxConfig.java
+++ src/main/java/pucrs/ages/garbus/Config/SpringFoxConfig.java
@@ -1,14 +1,21 @@
 package pucrs.ages.garbus.Config;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.AuthorizationScopeBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Collections;
 
 import static springfox.documentation.service.ApiInfo.DEFAULT_CONTACT;
@@ -19,11 +26,25 @@ public class SpringFoxConfig {
 
     @Bean
     public Docket api() {
+        AuthorizationScope[] authScopes = new AuthorizationScope[]{
+                new AuthorizationScopeBuilder().scope("global").description(
+                        "full access").build()};
+        SecurityReference securityReference = SecurityReference.builder().reference(
+                "Authorization-Key").scopes(authScopes).build();
+
+        ArrayList<SecurityContext> securityContexts = Lists.newArrayList(
+                SecurityContext.builder().securityReferences(Lists.newArrayList(
+                        securityReference)).build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securitySchemes(
+                        Lists.newArrayList(
+                                new ApiKey("Authorization-Key", "Authorization", "header")))
+                .securityContexts(securityContexts);
     }
 
     public static final ApiInfo DEFAULT_API_INFO = new ApiInfo (
