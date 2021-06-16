c4020c4fd807d4fd2d7cba5bc1c9883ebf471c4c
Ardel_Nunes <Ardel_Nunes@Dellteam.com>
2021-06-15 21:20:50 -0300

    Feat US 14 - change password Feito por: Ardel Junior e Lucas Soares

--- src/main/java/pucrs/ages/garbus/controllers/impl/AuthenticationControllerImpl.java
+++ src/main/java/pucrs/ages/garbus/controllers/impl/AuthenticationControllerImpl.java
@@ -1,17 +1,21 @@
 package pucrs.ages.garbus.controllers.impl;
 
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pucrs.ages.garbus.controllers.AuthenticationController;
-import pucrs.ages.garbus.dtos.JwtRequest;
-import pucrs.ages.garbus.dtos.JwtResponse;
-import pucrs.ages.garbus.dtos.PasswordRecoveryRequest;
-import pucrs.ages.garbus.dtos.PasswordRecoveryResponse;
+import pucrs.ages.garbus.dtos.*;
+import pucrs.ages.garbus.entities.Users;
 import pucrs.ages.garbus.excpetion.NotFoundException;
+import pucrs.ages.garbus.repositories.UsersRepository;
 import pucrs.ages.garbus.services.UsersAuthenticationService;
 
 import javax.annotation.Resource;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -21,6 +25,8 @@ public class AuthenticationControllerImpl implements AuthenticationController {
 
     @Resource
     private UsersAuthenticationService usersAuthenticationService;
+
+    private final UsersRepository usersRepository;
 
     @Override
     public ResponseEntity<JwtResponse> authenticate(JwtRequest jwtRequest) throws Exception {
@@ -35,6 +41,10 @@ public class AuthenticationControllerImpl implements AuthenticationController {
             e.printStackTrace();
             return new ResponseEntity<>(e.getError(), NOT_FOUND);
         }
+    }
 
+    @Override
+    public ResponseEntity<String> updatePassword(long userId, String password){
+        return new ResponseEntity<>(usersAuthenticationService.changePassword(userId, password), OK);
     }
 }
