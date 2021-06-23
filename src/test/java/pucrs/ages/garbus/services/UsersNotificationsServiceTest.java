dfdebbe83311576c87ed82ed79033cf29ab55bc4
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2021-06-23 00:28:20 -0300

    Testes de reativação de notificações

---
+++ src/test/java/pucrs/ages/garbus/services/UsersNotificationsServiceTest.java
@@ -0,0 +1,77 @@
+package pucrs.ages.garbus.services;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.BDDAssertions.then;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import pucrs.ages.garbus.entities.Users;
+import pucrs.ages.garbus.entities.UsersNotifications;
+import pucrs.ages.garbus.repositories.NotificationTokensRepository;
+import pucrs.ages.garbus.repositories.UsersNotificationsRepository;
+
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+class UsersNotificationsServiceTest {
+    @InjectMocks
+    private UsersNotificationsService usersNotificationsService;
+
+    @Mock
+    private UsersNotificationsRepository usersNotificationsRepository;
+
+    @Mock
+    private NotificationTokensRepository notificationTokensRepository;
+
+    @Mock
+    private UsersService usersService;
+
+    @BeforeEach
+    void setup() {
+        usersNotificationsService = new UsersNotificationsService(usersNotificationsRepository, notificationTokensRepository, usersService);
+    }
+
+    @Test
+    @DisplayName("Reativar notificações")
+    void reactivateNotifications() {
+        // Given
+        List<Users> users = generateUsers();
+        String login = users.get(0).getLogin();
+        LocalDateTime disabledUntil = LocalDateTime.now();
+
+        // When
+        given(usersNotificationsRepository.findByLogin(login)).willReturn(
+                Optional.ofNullable(
+                        UsersNotifications.builder().users(users.get(0)).disabledUntil(disabledUntil).build()
+                )
+        );
+        usersNotificationsService.reactivate(login);
+
+        // Then
+        Optional<UsersNotifications> nr = usersNotificationsRepository.findByLogin(login);
+        if (nr.isPresent()) {
+            then(nr.get().getDisabledUntil()).isNull();
+        } else {
+            fail("UsersNotifications not found");
+        }
+    }
+
+    private List<Users> generateUsers() {
+        List<Users> users = new ArrayList<>();
+        users.add(Users.builder().id(1).name("Test User").login("test").build());
+        return users;
+    }
+}
