f7a4a63381d5b93c6a3a68ebf34da565320a7d5f
Calebe Rocha <angelo.calebe@edu.pucrs.br>
2021-07-01 23:40:19 -0300

    Imports desnecessários removidos

--- src/main/java/pucrs/ages/garbus/mappers/TrashesEventsMapper.java
+++ src/main/java/pucrs/ages/garbus/mappers/TrashesEventsMapper.java
@@ -4,14 +4,11 @@ import lombok.AllArgsConstructor;
 import org.mapstruct.Mapper;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
-import pucrs.ages.garbus.dtos.EventsDTO;
 import pucrs.ages.garbus.dtos.TrashesEventsDTO;
-import pucrs.ages.garbus.entities.Events;
 import pucrs.ages.garbus.entities.TrashesEvents;
 import pucrs.ages.garbus.entities.Users;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Component
@@ -38,10 +35,6 @@ public class TrashesEventsMapper {
                         .build();
                 })
                 .collect(Collectors.toList());
-//        return source
-//                .stream()
-//                .map(entity -> modelMapper.map(entity, TrashesEventsDTO.class))
-//                .collect(Collectors.toList());
     }
 
     public TrashesEventsDTO mapear(TrashesEvents source) {
